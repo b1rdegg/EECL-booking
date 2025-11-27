0ecfffb02ee5914cabefd81f569040a067137006
Jhin <wgqq822qxq@privaterelay.appleid.com>
2025-11-27 13:32:04 +0800

    jj

--- EECL-booking/app.py
+++ EECL-booking/app.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 SCOPE = ['https://www.googleapis.com/auth/spreadsheets', 
          'https://www.googleapis.com/auth/drive']
 
-"""99998889"""
+"""33663017"""
 @st.cache_resource
 def init_connection():
     """初始化與 Google Sheets 的連線"""
